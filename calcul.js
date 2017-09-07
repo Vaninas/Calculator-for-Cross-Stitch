fb90a01da3790d0eecdc7105915b3127c02a144a
Vanina <vanina@gmail.com>
2017-09-07 10:45:42 +0200

    Modification for the error handling about the seizure of the height and the width calcul.js

--- calcul.js
+++ calcul.js
@@ -1,3 +1,39 @@
+//Surligne les champs faux
+ function surligne(champ, erreur) {
+   if(erreur) {
+      champ.style.backgroundColor = "#fba";
+      alert("La largeur et la hauteur des point doivent être compris entre 1 et 1000");
+      document.getElementById("nombrePointsL").focus();
+   } else {
+      champ.style.backgroundColor = "";
+   }
+}
+
+
+//Vérification que la largeur de point est en entier et un nombre entre 1 et 1000
+function verifLargeur(champ) {
+    var largeurVerif = parseInt(champ.value);
+    if(isNaN(largeurVerif) || largeurVerif <= 0 || largeurVerif > 1000) {
+       surligne(champ, true);
+       return false; 
+   } else {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+// Idem avec la hauteur
+function verifHauteur(champ) {
+    var hauteurVerif = parseInt(champ.value);
+    if(isNaN(hauteurVerif) || hauteurVerif <= 0 || hauteurVerif > 1000) {
+       surligne(champ, true);
+       return false;
+} else {
+      surligne(champ, false);
+      return true;
+   }
+}
+
  /*----------------------------------------------------*/
 //Calcul des dimensions nécessaires pour la toile au click sur le bouton
 document.getElementById("calculer").addEventListener("click", function () {   
